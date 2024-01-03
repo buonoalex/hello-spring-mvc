345f47f3d8b001f99716b19904e6f32ab79ed754
buonoalex <loopatthecity@gmail.com>
2024-01-03 16:57:13 +0100

    Initial commit

---
+++ src/main/java/org/lesson/hellospringmvc/HelloSpringMvcApplication.java
@@ -0,0 +1,14 @@
+package org.lesson.hellospringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringMvcApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HelloSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/lesson/hellospringmvc/controller/IndexController.java
@@ -0,0 +1,24 @@
+package org.lesson.hellospringmvc.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+import java.util.Random;
+
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping
+    public String index(Model model){
+        //Gemero un numero casuale
+        Random numberRandom = new Random();
+        int number = numberRandom.nextInt(1,7);
+        model.addAttribute("numberRandom",number);
+        return "home";
+    }
+
+}
